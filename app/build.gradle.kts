5b39a95f9346c8619f359190344d24a9ffe867f1
zowi <xxx@example.com>
2026-02-10 18:01:21 +0100

    Added room dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,14 @@ android {
 }
 
 dependencies {
+    implementation (libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.compose.runtime.livedata)
+
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
