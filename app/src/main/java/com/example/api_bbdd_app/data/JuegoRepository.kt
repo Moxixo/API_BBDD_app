482d2800a1b5ae659f18f35611ae10a6fb5bb945
zowi <xxx@example.com>
2026-02-19 20:44:31 +0100

    Dev repository game

--- app/src/main/java/com/example/api_bbdd_app/data/JuegoRepository.kt
+++ app/src/main/java/com/example/api_bbdd_app/data/JuegoRepository.kt
@@ -1,6 +1,9 @@
 package com.example.api_bbdd_app.data
 
+import com.example.api_bbdd_app.data.local.AppDatabase
+
 /**Clase repositorio
 * -> acceso a los datos (bbdd,api)
 * -> para conectar con la logica
 * que está a parte**/
+
---
+++ app/src/main/java/com/example/api_bbdd_app/data/local/JuegoRepositoryImpl.kt
@@ -0,0 +1,5 @@
+package com.example.api_bbdd_app.data.local
+
+class JuegoRepositoryImpl(
+    private val localDataSource : AppDatabase
+)
